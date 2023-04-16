58c72b0067357efc3a198c9a57bc59c780cafc88
Leon Rauschning <leon.rauschning@tum.de>
2023-04-16 06:58:17 +0200

    using libc now to kill threads

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::str::FromStr;
 use rip_shuffle::RipShuffleSequential;
-use std::sync::mpsc::{channel, TryRecvError};
+use std::sync::mpsc::channel;
 use std::thread;
+use std::os::unix::thread::JoinHandleExt; // only works on unix because Rust doesn't support
+                                          // killing threads on other platforms.
 
 
 fn main(){
@@ -10,40 +12,29 @@ fn main(){
     //println!("{}", issorted(&arr));
 
     let (send, recv) = channel();
-    let mut t_killers = Vec::new();
+    let mut threads = Vec::new();
 
     for _ in 0..8{
         let t_send = send.clone();
-        let mut t_arr = arr.clone();
-
-        // do not leave detached threads around
-        // (should probably be fine anyway, but
-        // cleaner this way)
-        let (t_kill_sender, t_kill_recv) = channel(); 
-        t_killers.push(t_kill_sender);
-        thread::spawn(move || {
-            while !issorted(&t_arr){
-                t_arr.seq_shuffle(&mut rand::thread_rng());
-                // shamelessly stolen from
-                // https://stackoverflow.com/questions/26199926/how-to-terminate-or-suspend-a-rust-thread-from-another-thread
-                match t_kill_recv.try_recv() {
-                    Ok(_) | Err(TryRecvError::Disconnected) => {
-                        return;
-                    }
-                    Err(TryRecvError::Empty) => {}
-                }
+        let t_arr = arr.clone();
+        threads.push(thread::spawn(move || {
+            if let Err(_) = t_send.send(bogosort(t_arr)) {
+                panic!("Error in sending result!");
             }
-            t_send.send(t_arr);
-        });
+        }));
     }
 
-    let sorted = recv.recv().unwrap();
-    // kill threads
-    for killer in t_killers {
-        killer.send(());
-    }
+    // wait for one of the workers to find the right solution, print it
+    println!("{:?}", recv.recv().unwrap());
 
-    println!("{:?}", sorted);
+    // cancel threads using libc
+    // should be fine not killing them, but cleaner this way
+    // using pthread_kill kills the entire process for some reason
+    unsafe {
+        for thread in threads {
+            libc::pthread_cancel(thread.into_pthread_t());
+        }
+    }
 }
 
 fn bogosort(mut arr:Vec<isize>) -> Vec<isize> {
